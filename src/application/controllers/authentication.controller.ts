fc80aeb023cb043cdf1b3c506edde75be15a75e3
Guilherme Ferreira <guilherme@primi.com.br>
2022-09-15 15:48:51 -0300

    t: testes com repositorio knex

--- src/application/controllers/authentication.controller.ts
+++ src/application/controllers/authentication.controller.ts
@@ -1,20 +1,32 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
 import { InjectKnex, Knex } from 'nestjs-knex';
 
 @Controller()
 export class AppController {
   constructor(@InjectKnex() private readonly knex: Knex) {}
 
-  @Get('vapo')
+  @Post('user')
   async getHello() {
-    if (!(await this.knex.schema.hasTable('users'))) {
-      await this.knex.schema.createTable('users', (table) => {
-        table.increments('id').primary();
-        table.string('name');
-      });
-    }
-    await this.knex.table('users').insert({ name: 'Name' });
-    const users = await this.knex.table('users');
-    return { users };
+    const gnvSealSupplies = await this.knex('filling_stations as fs')
+      .select({
+        company_name: 'fs.company_name',
+        street: 'a.street',
+        number: 'a.number',
+        neighborhood: 'a.neighborhood',
+        city_name: 'a.city_name',
+        state_name: 'a.state_name',
+        supply_at: 'vs.supply_at',
+      })
+      .innerJoin('valid_supplies as vs', 'fs.id', 'vs.filling_station_id')
+      .innerJoin('addresses as a', 'fs.address_id', 'a.id')
+      .whereNull('vs.finished_at')
+      .andWhere('vs.gnv_seal_chip_id', 1)
+      .andWhereRaw(`vs.supply_at >= '2022-01-01'`)
+      .andWhereRaw(`vs.supply_at <= '2022-09-15'`)
+      .limit(15)
+      // .offset((pag - 1) * 10)
+      .orderByRaw('vs.supply_at desc');
+
+    return { gnvSealSupplies };
   }
 }
